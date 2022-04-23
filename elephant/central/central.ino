0c7a6d957d23f33b74cd6f83b5cba4c140b8a771
Victor Hung <victorhung92@gmail.com>
2022-04-23 10:26:14 -0400

    feat: clean up central

--- elephant/central/central.ino
+++ elephant/central/central.ino
@@ -1,14 +1,29 @@
+/*
+  Elephant Central.ino Find the Bread Demo
+  
+  Acts as the 'central' (loaf)
+  [Victor Hung](vhung@mit.edu)
+*/
 
 #include <ArduinoBLE.h>
 
-int windowPointer = 0;
+
+// A larger window length means a more stable value,
+// but the value will take longer to get there.
 const int windowLength = 15;
+
+// A higher resolution means you'll get more granularity,
+// but too high of a resolution means there will be noise
+int resolution = 5;
+
+int windowPointer = 0;
 int window[windowLength];
+
 
 void setup() {
   Serial.begin(9600);
   delay(2000);
-  
+
   BLE.begin();
   BLE.setEventHandler(BLEDiscovered, discovered);
   BLE.setEventHandler(BLEDisconnected, blePeripheralDisconnectHandler);
@@ -34,24 +49,20 @@ void logDevice(BLEDevice dev) {
 void discovered(BLEDevice dev) {
   BLE.stopScan();
   logDevice(dev);
-  Serial.println("ATTEMPTING CONNECT");
-    if (dev.connect()) {
-      Serial.println("Connected");
-      while(dev.connected()) {
-        BLE.poll();
-        bluetoothLoop(dev.rssi());
-        delay(20);
-      }
-    } else {
-      Serial.println("Failed to connect!");
-      return;
+  Serial.println("Discovered, attempting to connect...");
+  if (dev.connect()) {
+    Serial.println("Connected!");
+    while (dev.connected()) {
+      // If connected, constantly run the 'bluetoothLoop'
+      BLE.poll();
+      bluetoothLoop(dev.rssi());
+      delay(20);
     }
-      BLE.scanForUuid("19b10000-e8f2-537e-4f6c-d104768a1214");
-//    
-//  digitalWrite(LED_BUILTIN, HIGH);
-//  delay(250); // ms
-//  digitalWrite(LED_BUILTIN, LOW);
-//  BLE.scanForUuid("19b10000-e8f2-537e-4f6c-d104768a1214");
+  } else {
+    Serial.println("Failed to connect!");
+    return;
+  }
+  BLE.scanForUuid("19b10000-e8f2-537e-4f6c-d104768a1214");
 }
 
 void blePeripheralDisconnectHandler(BLEDevice central) {
@@ -60,16 +71,18 @@ void blePeripheralDisconnectHandler(BLEDevice central) {
   Serial.println(central.address());
 }
 
+// This loop helps with the connection, but is then no longer used
+void loop() {
+  BLE.poll();
+}
+
+// This loop is used once the device is connected
 void bluetoothLoop(int signalStrength) {
   window[windowPointer] = signalStrength;
   windowPointer = (windowPointer + 1) % windowLength;
   int sum = 0;
-  for (int i = 0; i < windowLength; i ++) {
+  for (int i = 0; i < windowLength; i++) {
     sum += window[i];
   }
-  Serial.println(sum/windowLength/5 * -1);
-}
-
-void loop() {
-  BLE.poll();
+  Serial.println(sum / windowLength / resolution * -1);
 }
